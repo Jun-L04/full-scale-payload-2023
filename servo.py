18207f5f3034582f9022dbd9fa2f57f2f5620b50
Oori Schubert <61707814+oorischubert@users.noreply.github.com>
2022-11-08 00:40:44 -0500

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,6 +1,6 @@
 #Subscale Servo test
 #Written by Oori Schubert
-#Written in micropython
+#WLanguage: micropython
 #Makes servo rotate between 0 and 180 degrees
 
 
@@ -10,10 +10,11 @@ from time import sleep
 pwm = PWM(Pin(17))
 pwm.freq(50)
 
+#position 1000 = 0 degrees and position 9000 = 180 degrees
 while True:
-    for position in range(1000,9000,50): #position 1000 =0 degrees and position 9000 = 180 degrees
+    for position in range(1000,9000,50): #0 to 180
         pwm.duty_u16(position)
         sleep(0.01)
-    for position in range(9000,1000,-50):
+    for position in range(9000,1000,-50): #180 to 0
         pwm.duty_u16(position)
         sleep(0.01)
